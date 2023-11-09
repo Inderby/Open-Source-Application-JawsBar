c4cbcd7b6282d3472b0ef230ee4310ff866d92a6
JiHun <pgh979@gmail.com>
2023-11-09 21:00:11 +0900

    파일명 변경, 엠프티 함수 소스코드 베이스 추가

---
+++ source/empty.cc
@@ -0,0 +1,10 @@
+/**
+ * Copyright OpenSourceApplication-JawsBar.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Created by 12161580ParkJiHun on 2023/11/09.
+ */
+
+#include "../header/AVL_tree.h"
