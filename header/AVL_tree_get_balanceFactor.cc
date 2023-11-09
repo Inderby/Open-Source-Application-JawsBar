4ff0ce443cb52543038a0f0651e6c31e4606ec50
Inderby <youk8253@gmail.com>
2023-11-09 22:26:23 +0900

    설명을 위한 주석 추가

--- header/AVL_tree_get_balanceFactor.cc
+++ header/AVL_tree_get_balanceFactor.cc
@@ -8,7 +8,7 @@
  */
 
 #include "AVL_tree.h"
-
+/*인자로 들어온 노드에 양쪽 자식의 높이 차이를 반환해주는 기능*/
 int AVL_Tree::GetBalanceFactor(Node *node_to_check)
 {
   return GetHeight(node_to_check->left_child) - GetHeight(node_to_check->right_child);
