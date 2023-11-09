99328d786a4935d4efb28d1c10b02b30d9c256f2
JiHun <pgh979@gmail.com>
2023-11-09 22:46:22 +0900

    공백 추가

--- source/empty.cc
+++ source/empty.cc
@@ -12,9 +12,9 @@
 // 엠프티 함수 선언문
 int Empty(){
   // 사이즈가 0이면 1 반환, 0이 아니고 양수면 0 반환, 음수면 에러 반환
-  if(this->Size() == 0){
+  if(Size() == 0){
     return 1;
-  } else if(this->Size() > 0){
+  } else if(Size() > 0){
     return 0;
   } else {
     return -1;
