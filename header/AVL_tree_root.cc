bfe79191423509f8883d5f5c84639503a1aca90e
Inderby <youk8253@gmail.com>
2023-11-09 22:20:24 +0900

    트리의 `root_`를 반환해주는 접근자 함수 구현

---
+++ header/AVL_tree_root.cc
@@ -0,0 +1,14 @@
+/**
+ * Copyright OpenSourceApplication-JawsBar.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Created by 12191572KimYongBin on 2023/11/09.
+ */
+
+#include "AVL_tree.h"
+
+Node *AVL_Tree::Root(){
+  return this->root_;
+}
