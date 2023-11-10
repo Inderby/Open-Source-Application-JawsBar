8859baefcb6d5bdb418c164407b90b5a4aaa0bd4
Inderby <youk8253@gmail.com>
2023-11-10 11:41:01 +0900

    설명 주석 추가 및 코드 포맷 수정(기능 변경 X)

--- header/AVL_tree_insert.cc
+++ header/AVL_tree_insert.cc
@@ -9,18 +9,25 @@
 
 #include "AVL_tree.h"
 
+/*인자로 들어온 값을 내부적으로 InternalInsert를 호출해 삽입하고 depth를 반환하는 함수*/
 int AVL_Tree::Insert(int value_to_insert)
 {
-  if(this->size_ == 0){
+  /*Tree가 비어 있는 경우 바로 삽입 수행*/
+  if(this->size_ == 0)
+  { 
     this->root_ = InternalInsert(this->root_, value_to_insert);
       return 0;
   }
-  else{
+  else
+  {
+    /*root의 key값과 동일할 경우 해당 depth인 0을 반환*/
     if(value_to_insert == this->root_->key)
       return 0;
 
     int depth_of_new_node = 0;
-    if((depth_of_new_node =Find(value_to_insert)) == 0){
+    /*해당 value가 Tree내에 존재하지 않을 경우 수행*/
+    if((depth_of_new_node = Find(value_to_insert)) == 0)
+    {
       this->root_ = InternalInsert(this->root_, value_to_insert);
       depth_of_new_node = Find(value_to_insert);
     }
