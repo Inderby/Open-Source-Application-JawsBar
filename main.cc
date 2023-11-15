4704e37261f3a6a36bae4e66c02dea62b72c799d
Inderby <youk8253@gmail.com>
2023-11-15 12:58:38 +0900

    파일 상단에 MIT 라이선스 주석 추가

--- main.cc
+++ main.cc
@@ -1,3 +1,11 @@
+/**
+ * Copyright OpenSourceApplication-JawsBar.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Created by 12191572KimYongBin on 2023/11/14.
+ */
 #include "header/AVL_tree.h"
 #include <stdio.h>
 #include <string>
