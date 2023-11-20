9146b450743894f794275b4657ced383feaf7c41
JiHun <pgh979@gmail.com>
2023-11-20 17:07:50 +0900

    rank 라이선스 및 인클루드 추가

---
+++ header/AVL_tree_rank.cc
@@ -0,0 +1,39 @@
+/**
+ * Copyright OpenSourceApplication-JawsBar.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Created by 12161580ParkJiHun on 2023/11/20.
+ */
+
+#include "AVL_tree.h"
+
+/*
+// key값을 가진 Node의 depth를 출력하는 함수임.
+int AVLTree::Find(int value_to_find) {
+  Node *node_to_search = root_;
+  int depth_of_find = 0;
+
+  // key값을 찾거나 없다면 break함.
+  while (node_to_search != nullptr && node_to_search->key != value_to_find) {
+    node_to_search = (value_to_find < node_to_search->key)
+                         ? node_to_search->left_child
+                         : node_to_search->right_child;
+    depth_of_find++;
+  }
+
+  // 찾고자 하는 노드를 찾지 못하면 0을 반환함.
+  if (node_to_search == nullptr) {
+    if (GetIsPrint()) {
+      std::cout << 0 << "\n";
+    }
+    return 0;
+  }
+
+  if (GetIsPrint()) {
+    std::cout << depth_of_find << "\n";
+  }
+  return depth_of_find;
+}
+*/
