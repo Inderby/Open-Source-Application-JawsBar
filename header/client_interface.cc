2342253b0b398965266773a3869b23a1069d23f3
Inderby <youk8253@gmail.com>
2023-12-11 20:09:42 +0900

    `client_interface`클래스 생성자 정의

---
+++ header/client_interface.cc
@@ -0,0 +1,13 @@
+/**
+ * Copyright OpenSourceApplication-JawsBar.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Created by 12191572KimYongBin on 2023/12/11.
+ */
+
+#include "client_interface.h"
+
+// 생성자 선언
+ClientInterface::ClientInterface(AVLTree *tree) : tree_(tree) {}
